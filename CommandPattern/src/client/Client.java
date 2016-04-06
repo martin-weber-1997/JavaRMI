6c71f3302996cb1a7e649feaa4d1bd35756499b0
Martin Weber <mweber02@student.tgm.ac.at>
2016-04-06 20:32:42 +0200

    Added Command Pattern and CalcPi Command

--- CommandPattern/src/client/Client.java
+++ CommandPattern/src/client/Client.java
@@ -4,7 +4,10 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
+import calculation.Calculation;
+import calculation.PICalc;
 import remoteService.DoSomethingService;
+import server.commands.CalculationCommand;
 import server.commands.Command;
 import server.commands.RegisterCommand;
 import server.commands.LoginCommand;
@@ -23,9 +26,13 @@ public class Client {
 
 			Command rc = new RegisterCommand();
 			Command lc = new LoginCommand();
+
+			Calculation pi = new PICalc(Integer.parseInt(args[0]));
+
+			Command pc = new CalculationCommand(pi);
 			uRemoteObject.doSomething(rc);
 			uRemoteObject.doSomething(lc);
-
+			uRemoteObject.doSomething(pc);
 		} catch (RemoteException re) {
 			System.err.println("Service not found?" + " Check your RMI-Registry!");
 			System.exit(1);
--- CommandPattern/src/server/commands/CalculationCommand.java
+++ CommandPattern/src/server/commands/CalculationCommand.java
@@ -2,16 +2,24 @@ package server.commands;
 
 import java.io.Serializable;
 import calculation.Calculation;
-
+/**
+ * 
+ * @author Martin Weber, Michael Borko
+ * @version 06.04.2016
+ *
+ */
 public class CalculationCommand implements Command, Serializable {
 
 	private static final long serialVersionUID = 3202369269194172790L;
 	private Calculation calc;
 
+	public CalculationCommand(Calculation calc) {
+		this.calc = calc;
+	}
+
 	@Override
 	public void execute() {
-		System.out.println("CalculationCommand called!");
 		calc.calculate();
-		calc.getResult();
+		System.out.println("CalculationCommand called! " +calc.getResult().toString());
 	}
 }
